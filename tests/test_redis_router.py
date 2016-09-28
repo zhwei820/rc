9333ab475f18e5f655f814653369c39f6e9ed135
weizhou <602068971@qq.com>
2016-09-28 23:01:23 +0800

    test

--- tests/test_redis_router.py
+++ tests/test_redis_router.py
@@ -13,13 +13,16 @@ def test_redis_router_basics():
 
     assert router.get_key_for_command('GET', 'c') == 'c'
     assert router.get_key_for_command('SET', 'g') == 'g'
+    assert router.get_key_for_command('SET', 'n') == 'n'
     with pytest.raises(RuntimeError):
-        router.get_key_for_command('MGET', ['a', 'b', 'c'])
+        router.get_key_for_command('MGET', ['a', 'b', 'c', 'n', 'm'])
 
     assert router.get_host_for_key('c') == 0
     assert router.get_host_for_key('g') == 1
-    assert router.get_host_for_key('a') == 2
+    assert router.get_host_for_key('a') == 0
+    assert router.get_host_for_key('n') == 0
+    assert router.get_host_for_key('m') == 2
 
     assert router.get_host_for_command('GET', 'c') == 0
     assert router.get_host_for_command('GET', 'g') == 1
-    assert router.get_host_for_command('SET', 'a') == 2
+    assert router.get_host_for_command('SET', 'a') == 0
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,23 +5,23 @@ def test_generate_key():
     def func():
         pass
     cache_key = generate_key_for_cached_func(None, func, 'foo')
-    assert cache_key == u'test_utils func foo'
+    assert cache_key == u'test_utils_func_foo'
     cache_key = generate_key_for_cached_func('prefix', func, 'foo')
-    assert cache_key == u'prefix test_utils func foo'
+    assert cache_key == u'prefix_test_utils_func_foo'
     cache_key = generate_key_for_cached_func(None, func, 'foo', k='v')
-    assert cache_key == u'test_utils func foo k=v'
+    assert cache_key == u'test_utils_func_foo'
     cache_key = generate_key_for_cached_func(None, func,
                                              'foo', k='v', k2='v2')
-    assert cache_key == u'test_utils func foo k=v k2=v2'
+    assert cache_key == u'test_utils_func_foo'
 
     def method(self):
         pass
     cache_key = generate_key_for_cached_func(None, method, 'foo')
-    assert cache_key == u'test_utils method foo'
+    assert cache_key == u'test_utils_method_foo'
     cache_key = generate_key_for_cached_func(None, method, None, 'foo')
-    assert cache_key == u'test_utils method None foo'
+    assert cache_key == u'test_utils_method_None_foo'
 
     def method(cls):
         pass
     cache_key = generate_key_for_cached_func(None, method, 'foo')
-    assert cache_key == u'test_utils method foo'
+    assert cache_key == u'test_utils_method_foo'
